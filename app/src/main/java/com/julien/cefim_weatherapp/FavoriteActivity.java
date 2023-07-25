da18b1c613d1c759860ad86c43f782b2c0edb803
Julien Moineau <moineau.julien@hotmail.fr>
2023-07-25 11:49:38 +0200

    TP 3 - Vue dynamique - RecyclerView

--- app/src/main/java/com/julien/cefim_weatherapp/FavoriteActivity.java
+++ app/src/main/java/com/julien/cefim_weatherapp/FavoriteActivity.java
@@ -1,5 +1,8 @@
 package com.julien.cefim_weatherapp;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -8,20 +11,35 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 
+import com.julien.cefim_weatherapp.adapters.FavoriteAdapter;
 import com.julien.cefim_weatherapp.databinding.ActivityFavoriteBinding;
+import com.julien.cefim_weatherapp.models.City;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class FavoriteActivity extends AppCompatActivity {
 
     private ActivityFavoriteBinding binding; // Permet de récupérer toute la vue (et donc ses membres) --> évite de créer une variable pour chaque membre
+    private ArrayList<City> mCities;
+    private Context mContext;
+    private RecyclerView mRecyclerView;
+    private FavoriteAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mContext = this;
+
+        init();
+
         super.onCreate(savedInstanceState);
 
         Log.d("lol", "Activité Favoris créée chef, c'est de toute beauté !");
@@ -36,20 +54,41 @@ public class FavoriteActivity extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
 
+        mRecyclerView = binding.include.myRecyclerView;
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new FavoriteAdapter(mContext, mCities);
+        mRecyclerView.setAdapter(mAdapter);
+
         FloatingActionButton fab = binding.fab;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Je garde le snackbar juste pour avoir un exemple d'utilisation de snackbar
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+
+                View v = LayoutInflater.from(mContext).inflate(R.layout.dialog_add_favorite, null);
+                builder.setView(v);
+
+                builder.setPositiveButton(android.R.string.ok, (dialogInterface, i) -> {
+                    addCityDialog(v);
+                });
+                builder.setNegativeButton(android.R.string.cancel, null);
+                builder.create().show();
+            }
+
+            private void addCityDialog(View v) {
+                EditText editText = v.findViewById(R.id.edit_text_dialog_city);
+                String cityName = editText.getText().toString();
+                City city = new City(cityName, "Ensoleillé", "22°C", R.drawable.weather_sunny_grey);
+                mCities.add(0, city); // L'index à 0 pour que la nouvelle ville apparaisse en haut de la liste
+                mAdapter.notifyDataSetChanged();
             }
         });
-
-        Bundle extras = getIntent().getExtras();
-        if (extras !=null) {
-            assert binding.include.textViewMessage != null;
-            binding.include.textViewMessage.setText("Message : " + extras.getString("message", getString(R.string.favorites_text_view_default)));
-        }
     }
 
     @Override
@@ -80,5 +119,83 @@ public class FavoriteActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         Log.d("lol", "Activité Favoris arrêtée ! On n'en a pas besoin pour l'instant.");
+    }
+
+    private void init() {
+        mCities = new ArrayList<>();
+        City city1 = new City("Montréal", "Légères pluies", "22°C", R.drawable.weather_rainy_grey);
+        City city2 = new City("New York", "Ensoleillé", "22°C", R.drawable.weather_sunny_grey);
+        City city3 = new City("Paris", "Nuageux", "24°C", R.drawable.weather_foggy_grey);
+        City city4 = new City("Toulouse", "Pluies modérées", "20°C", R.drawable.weather_rainy_grey);
+        City city5 = new City("Dunkerque", "Gris", "13°C", R.drawable.weather_foggy_grey);
+        City city6 = new City("Rennes", "Ensoleillé", "23°C", R.drawable.weather_sunny_grey);
+        City city7 = new City("Niort", "Déprime", "14°C", R.drawable.weather_foggy_grey);
+        City city8 = new City("Molsheim", "Pluies modérées", "20°C", R.drawable.weather_rainy_grey);
+
+        // Beaucoup de villes répétées juste pour avoir l'effet de défilement
+        mCities.add(city1);
+        mCities.add(city2);
+        mCities.add(city3);
+        mCities.add(city4);
+        mCities.add(city5);
+        mCities.add(city6);
+        mCities.add(city7);
+        mCities.add(city8);
+        mCities.add(city1);
+        mCities.add(city2);
+        mCities.add(city3);
+        mCities.add(city4);
+        mCities.add(city5);
+        mCities.add(city6);
+        mCities.add(city7);
+        mCities.add(city8);
+        mCities.add(city1);
+        mCities.add(city2);
+        mCities.add(city3);
+        mCities.add(city4);
+        mCities.add(city5);
+        mCities.add(city6);
+        mCities.add(city7);
+        mCities.add(city8);
+        mCities.add(city1);
+        mCities.add(city2);
+        mCities.add(city3);
+        mCities.add(city4);
+        mCities.add(city5);
+        mCities.add(city6);
+        mCities.add(city7);
+        mCities.add(city8);
+        mCities.add(city1);
+        mCities.add(city2);
+        mCities.add(city3);
+        mCities.add(city4);
+        mCities.add(city5);
+        mCities.add(city6);
+        mCities.add(city7);
+        mCities.add(city8);
+        mCities.add(city1);
+        mCities.add(city2);
+        mCities.add(city3);
+        mCities.add(city4);
+        mCities.add(city5);
+        mCities.add(city6);
+        mCities.add(city7);
+        mCities.add(city8);
+        mCities.add(city1);
+        mCities.add(city2);
+        mCities.add(city3);
+        mCities.add(city4);
+        mCities.add(city5);
+        mCities.add(city6);
+        mCities.add(city7);
+        mCities.add(city8);
+        mCities.add(city1);
+        mCities.add(city2);
+        mCities.add(city3);
+        mCities.add(city4);
+        mCities.add(city5);
+        mCities.add(city6);
+        mCities.add(city7);
+        mCities.add(city8);
     }
 }
---
+++ app/src/main/java/com/julien/cefim_weatherapp/adapters/FavoriteAdapter.java
@@ -0,0 +1,90 @@
+package com.julien.cefim_weatherapp.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.julien.cefim_weatherapp.R;
+import com.julien.cefim_weatherapp.models.City;
+
+import java.util.ArrayList;
+
+public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.ViewHolder> {
+
+    private Context mContext;
+    private ArrayList<City> mCities;
+    private int counter;
+
+    //Constructor
+    public FavoriteAdapter(Context mContext, ArrayList<City> mCities) {
+        this.counter = 1;
+        this.mContext = mContext;
+        this.mCities = mCities;
+    }
+
+    //Classe holder qui contient la vue d'un item
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
+        public TextView mTextViewCityName;
+        public TextView mTextViewDescription;
+        public TextView mTextViewTemperature;
+        public ImageView mImageView;
+        public City mCity;
+        public Integer mId; // Sert uniquement pour démontrer le nombre de views créées dans le log
+
+        public ViewHolder(View view) {
+            super(view);
+            mTextViewCityName = view.findViewById(R.id.item_favorite_text_view_city_name);
+            mTextViewDescription = view.findViewById(R.id.item_favorite_text_view_description);
+            mTextViewTemperature = view.findViewById(R.id.item_favorite_text_view_temperature);
+            mImageView = view.findViewById(R.id.item_favorite_image_view);
+            view.setOnLongClickListener(this);
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            Log.d("lol", "Clic long détecté sur l'item n°" + this.mId + " : " + this.mCity.mName + " chef !");
+            mCities.remove(this.mCity);
+            notifyItemRemoved(getAdapterPosition());
+            return false;
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("lol", "Création de la vue n°" + counter + " !");
+
+        View v = LayoutInflater.from(mContext).inflate(R.layout.item_favorite_city, parent, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+
+        viewHolder.mId = counter;
+        counter++;
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        City city = mCities.get(position);
+
+        Log.d("lol", "Vue n°" + holder.mId + " revêtue pour la ville " + city.mName);
+
+        holder.mTextViewCityName.setText(city.mName);
+        holder.mTextViewDescription.setText(city.mDescription);
+        holder.mTextViewTemperature.setText(city.mTemperature);
+        holder.mImageView.setImageResource(city.mWeatherIcon);
+        holder.mCity = city;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCities.size();
+    }
+}
