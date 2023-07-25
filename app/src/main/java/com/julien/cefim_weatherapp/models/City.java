d3a749ed958d7a295dd2bf76b65b9b74101080a8
Julien Moineau <moineau.julien@hotmail.fr>
2023-07-25 16:48:04 +0200

    TP 4 - Appel API --> Stockage des données

--- app/src/main/java/com/julien/cefim_weatherapp/models/City.java
+++ app/src/main/java/com/julien/cefim_weatherapp/models/City.java
@@ -1,16 +1,29 @@
 package com.julien.cefim_weatherapp.models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class City {
 
+    public int mIdCity;
     public String mName;
+    public double mLatitude;
+    public double mLongitude;
+
     public String mDescription;
     public String mTemperature;
     public int mWeatherIcon;
+    public int mWeatherResIconWhite;
 
     public City(String mName, String mDescription, String mTemperature, int mWeatherIcon) {
         this.mName = mName;
         this.mDescription = mDescription;
         this.mTemperature = mTemperature;
         this.mWeatherIcon = mWeatherIcon;
+    }
+
+    public City(String stringJson) throws JSONException {
+        JSONObject json = new JSONObject(stringJson);
+        mName = json.getString("name");
     }
 }
